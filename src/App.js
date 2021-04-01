8ec3beb96d50aab6b2cb24ba91384d105ed19237
MarioKrstevski <mariokrstevski@hotmail.com>
2021-04-01 02:56:44 +0200

    UPD App works
    - CSS update needed
    - Redux implementation needed refactor

--- src/App.js
+++ src/App.js
@@ -1,42 +1,101 @@
-import './App.scss';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import WeeklyCalendar from './components/WeeklyCalendar';
+import "./App.scss";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import moment from "moment";
+import WeeklyCalendar from "./components/WeeklyCalendar";
 
-const API_KEY = 'f5fb197fb552a47dc301c0a0327cffa6'
+const API_KEY = "f5fb197fb552a47dc301c0a0327cffa6";
+const API_URL =
+  "https://wozmx9dh26.execute-api.eu-west-1.amazonaws.com/api/holidays";
 
 function App() {
+  window.moment = moment;
 
-  const [holidays, setHolidays] = useState([]);
+  const [holidays, setHolidays] = useState({});
+  const [timelineCoverage, setTimelineCoverage] = useState([]);
+  const [neededDates, setNeededDates] = useState([]);
 
-  async function getHolidays(){
-    try {
-      const response = await axios.post('https://wozmx9dh26.execute-api.eu-west-1.amazonaws.com/api/holidays', {
-          "apiKey": API_KEY,
-          "startDate": "2019-02-01",
-          "endDate": "2019-02-28"
-      })
-      console.log('Holidays resp:', response)
-      console.log('Holidays resp DATA Holydays:', response.data.holidays)
-      setHolidays(response.data)
-    } catch (e) {
-      console.log('E',e)
-      if(e && e.response){
-        console.log(e.response.data)
-      }
+  const monthYearFormat = "MM.YYYY";
+  const APIDateFormat = "YYYY-MM-DD";
+
+  async function getHolidays(neededDates) {
+    axios
+      .all(
+        neededDates.map((monthYear) => {
+          const currentMonthYearObject = moment()
+            .month(monthYear.substr(0, 2) - 1)
+            .year(monthYear.substr(3));
+          const startOfMonth = currentMonthYearObject
+            .clone()
+            .startOf("month")
+            .format(APIDateFormat);
+          const endOfMonth = currentMonthYearObject
+            .clone()
+            .endOf("month")
+            .format(APIDateFormat);
+
+          return axios.post(API_URL, {
+            apiKey: API_KEY,
+            startDate: startOfMonth,
+            endDate: endOfMonth,
+          });
+        })
+      )
+      .then(
+        axios.spread((...responses) => {
+          let allHolidays = {};
+          responses.forEach((resp) => {
+            Object.assign(allHolidays, resp.data.holidays);
+          });
+          const updatedHolidays = {...holidays, ...allHolidays}
+          setHolidays(updatedHolidays);
+          console.log(updatedHolidays)
+          setTimelineCoverage([...timelineCoverage, ...neededDates]);
+          setNeededDates([]);
+        })
+      );
+  }
+
+  function notifyUpdate(dates) {
+    const monthYearFormatedDates = dates.map((momentObj) =>
+      momentObj.format(monthYearFormat)
+    );
+    const removedDuplicates = [...new Set(monthYearFormatedDates)];
+    const newNeededDates = removedDuplicates.filter(
+      (e) => !timelineCoverage.includes(e)
+    );
+    // console.log("newNeededDates", newNeededDates);
+    // console.log("timelineCoverage ", timelineCoverage);
+    if (newNeededDates.length) {
+      setNeededDates(newNeededDates);
     }
   }
 
   useEffect(() => {
-    getHolidays()
+    const currentDate = moment();
+    const nextMonth = moment(currentDate).add(30, "days");
+    const previousMonth = moment(currentDate).add(-30, "days");
+    const neededDates = [
+      currentDate.format(monthYearFormat),
+      nextMonth.format(monthYearFormat),
+      previousMonth.format(monthYearFormat),
+    ];
+    setNeededDates(neededDates);
   }, []);
+
+  useEffect(() => {
+    if (neededDates.length) {
+      getHolidays(neededDates);
+    }
+  }, [neededDates]);
 
   return (
     <div className="App">
       Hello
-      <WeeklyCalendar>
-
-      </WeeklyCalendar>
+      <WeeklyCalendar
+        holidays={holidays}
+        notifyUpdate={notifyUpdate}
+      ></WeeklyCalendar>
     </div>
   );
 }
--- src/components/WeeklyCalendar.js
+++ src/components/WeeklyCalendar.js
@@ -1,10 +1,14 @@
 import moment from "moment";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FiArrowLeft, FiArrowRight } from "react-icons/fi";
 
-const WeeklyCalendar = () => {
+const WeeklyCalendar = ({ notifyUpdate, holidays }) => {
+  // console.log("HD", holidays)
   const [startDay, setStartDay] = useState("Monday");
+  const [weekdaysWithDate, setWeekdaysWithDate] = useState([]);
   const [currentDate, setCurrentDate] = useState(moment());
+  const APIDateFormat = "YYYY-MM-DD";
+
   const dateFormatting = "DD.MM.YYYY";
   const weekdays = [
     "Monday",
@@ -22,7 +26,12 @@ const WeeklyCalendar = () => {
   function handleSetPreviousWeek() {
     setCurrentDate(moment(currentDate).days(-7));
   }
-  function renderDays() {
+
+  function handleStartDayChange(e) {
+    setStartDay(e.target.value);
+  }
+
+  useEffect(() => {
     const weekDaysShifted = [...weekdays];
     while (weekDaysShifted[0] !== startDay) {
       weekDaysShifted.push(weekDaysShifted.shift());
@@ -33,16 +42,43 @@ const WeeklyCalendar = () => {
     );
     const weekDaysShiftedWithDates = weekDaysShifted.map((day, idx) => ({
       name: day,
-      date: moment(currentDate)
-        .add(idx - currentDayPostion, "days")
-        .format(dateFormatting),
+      date: moment(currentDate).add(idx - currentDayPostion, "days"),
     }));
-    return weekDaysShiftedWithDates.map((day, index) => (
-      <div className="weekday">
+    setWeekdaysWithDate(weekDaysShiftedWithDates);
+  }, [currentDate, startDay]);
+
+  useEffect(() => {
+    if (weekdaysWithDate.length) {
+      notifyUpdate([...weekdaysWithDate.map((day) => day.date)]);
+    }
+  }, [weekdaysWithDate]);
+
+  function renderDaysHeaders() {
+    return weekdaysWithDate.map((day, index) => (
+      <div className="weekday" key={day.name}>
         <div className="weekday__name">{day.name}</div>
-        <div className="weekday__date">{day.date}</div>
+        <div className="weekday__date">{day.date.format(dateFormatting)}</div>
       </div>
     ));
+  }
+
+  function renderDaysContent() {
+    return weekdaysWithDate.map((day, idx) => {
+      const match = holidays[day.date.format(APIDateFormat)];
+      return (
+        <div className="weekday__holidays" key={day.name}>
+          {match &&
+            match.map((holiday) => (
+              <div
+                key={holiday.name + holiday.type}
+                className={`type-${holiday.type}`}
+              >
+                {holiday.name}
+              </div>
+            ))}
+        </div>
+      );
+    });
   }
 
   return (
@@ -55,13 +91,11 @@ const WeeklyCalendar = () => {
           <label htmlFor="startDay">Start Day: </label>
           <select
             value={startDay}
-            onChange={(e) => {
-              setStartDay(e.target.value);
-            }}
+            onChange={handleStartDayChange}
             name="startDay"
           >
             {weekdays.map((day) => (
-              <option>{day}</option>
+              <option key={day}>{day}</option>
             ))}
           </select>
         </div>
@@ -69,8 +103,8 @@ const WeeklyCalendar = () => {
           <FiArrowRight onClick={handleSetNextWeek} />
         </div>
       </div>
-      <div className="calendar__header">{renderDays()}</div>
-      <div className="calendar__body">{currentDate.format(dateFormatting)}</div>
+      <div className="calendar__header">{renderDaysHeaders()}</div>
+      <div className="calendar__body">{renderDaysContent()}</div>
     </div>
   );
 };
